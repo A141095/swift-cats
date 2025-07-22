0597b4dd4e1f99e1bc51bcca6feb7d675120a83c
Brenton Currie <bcurrie@agl.com.au>
2025-07-23 09:51:16 +1000

    Add RxSwift package

--- SwiftCats/NetworkService.swift
+++ SwiftCats/NetworkService.swift
@@ -2,6 +2,7 @@ import Foundation
 
 final class NetworkService {
     func getOwners(completionHandler: @escaping ([Owner]) -> Void) {
+        // TODO: Replace with RxSwift implementation
         let url = URL(string: "https://agl-developer-test.azurewebsites.net/people.json")!
         URLSession.shared.dataTask(with: url) { data, _, _ in
             guard let data else { return }
