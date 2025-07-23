a25d8c0c062e1fcf9764e13b868e50ac711e0264
Brenton Currie <bcurrie@agl.com.au>
2025-07-23 10:12:53 +1000

    Add additional logging

--- SwiftCats/ViewController.swift
+++ SwiftCats/ViewController.swift
@@ -4,6 +4,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         NetworkService().getOwners { (owners) in
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .short
+            print("Fetched owners at \(dateFormatter.string(from: .now))")
             print(owners)
         }
     }
