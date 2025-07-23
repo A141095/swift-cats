c2c507dbe9c41ad71748dc3667a27bc6fd148e7b
Brenton Currie <bcurrie@agl.com.au>
2025-07-23 13:53:48 +1000

    Clean up method

--- SwiftCats/ViewController.swift
+++ SwiftCats/ViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class ViewController: UIViewController {
     override func viewDidLoad() {
-        super.viewDidLoad()
         NetworkService().getOwners { (owners) in
             let dateFormatter = DateFormatter()
             dateFormatter.dateStyle = .medium
